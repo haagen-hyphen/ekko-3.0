7931909d8a448a162deab971fc47e36badca3329
hongttisme <76058140+hongttisme@users.noreply.github.com>
2024-06-12 21:08:00 +0800

    sprite added

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveCd;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
