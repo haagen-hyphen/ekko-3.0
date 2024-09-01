6563c93e394a33280c4d5f621b66b57e73e73a02
Hosef <joseph.txh@gmail.com>
2024-09-02 00:26:16 +0800

    initial ui commit
    basic main menu and level selection. In game allow pausing game, restart level, and return to main menu.
    
    Bug: skipped many frames when scene first loaded

--- Assets/Scripts/UIRelated/UIManager.cs
+++ Assets/Scripts/UIRelated/UIManager.cs
@@ -7,17 +7,18 @@ using Unity.VisualScripting;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public TickManager tickManager;
-    public RectTransform axisTransform;
+    // public RectTransform axisTransform;
     float t = 0f;
     public RectTransform abilitySlot;
     public Sprite hand;
     public Camera mainCamera;
     public bool isGamePaused; //to be put at every script's update
-    public AudioSource audioSource; 
+    // public AudioSource audioSource; 
 
     void Start()
     {
@@ -26,7 +27,7 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        RunTimeline();
+        // RunTimeline();
         if(Input.GetKeyDown(KeyCode.Escape) && !isGamePaused){
             PauseGame();
         }
@@ -39,15 +40,24 @@ public class UIManager : MonoBehaviour
     {
         Time.timeScale = 0;
         isGamePaused = true;
-        audioSource.enabled = false;
+        // audioSource.enabled = false;
         gameObject.transform.GetChild(1).gameObject.SetActive(true);
     }
     public void ResumeGame()
     {
         gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        audioSource.enabled = true;
+        // audioSource.enabled = true;
         isGamePaused = false;
         Time.timeScale = 1;
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 
     public void SetAbilityImage(Cell killedBy){
@@ -57,14 +67,14 @@ public class UIManager : MonoBehaviour
         abilitySlot.GetComponent<UnityEngine.UI.Image>().sprite = image;
     }
 
-    public void RunTimeline(){
-        t += 1f * Time.deltaTime;
-        if(t>=1){
-            t -= 1f;
-        }
-        float x = Mathf.Lerp(0,-50,t);
-        axisTransform.anchoredPosition = new Vector3(x,0,0);
-    }
+    // public void RunTimeline(){
+    //     t += 1f * Time.deltaTime;
+    //     if(t>=1){
+    //         t -= 1f;
+    //     }
+    //     float x = Mathf.Lerp(0,-50,t);
+    //     axisTransform.anchoredPosition = new Vector3(x,0,0);
+    // }
 
     public IEnumerator moveCamera(Vector3 towardsDirection, float inHowmanySeconds){
         int animationFluency = 100;
